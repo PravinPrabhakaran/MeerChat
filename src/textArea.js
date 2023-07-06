bc84b0ada7ee96f2fc47c9ab63b2b3043c8360db
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2023-07-06 11:40:24 +0100

    basic

---
+++ src/textArea.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function textArea() {
+    return (
+        <Container>
+          <Row>
+            <Col>
+              <h1>CTM-GPT</h1>
+              <textArea />
+              
+            </Col>
+          </Row>
+        </Container>
+      );
+    }
+  
