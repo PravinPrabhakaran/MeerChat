da0761960c3d0721af227d6637f76d7e314c6b37
fahadhewad <fahadhewad@gmail.com>
2023-07-07 15:29:45 +0100

    Updated frontend UI

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import Chat from './Chat'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import SergeiImage from './sergei.png';
+import AlekImage from './alek.png';
 
 function App() {
 
-  const [messages, setMessages]  = useState([["Asdasdoaskdoaskdasd", false]]);
+  const [messages, setMessages]  = useState([["Hi", false]]);
   
   const submitPrompt = () => {
     var prompt = document.getElementsByName("prompt")[0].value 
     console.log(prompt)
-    if (prompt == '') {
+    if (prompt === '') {
       return;
     }
 
@@ -22,27 +25,36 @@ function App() {
     // clear the message after logging it or using it
   };
 
-  
   return (
     <Container>
       <Row>
         <h1>CTM-GPT</h1>
       </Row>
-      <Form>
-        <Container>
-          {messages.map((message, key) => (
-            <Chat userPrompt={message[0]} user={message[1]} />
-          ))}
-        </Container>
-        <Row>
-          <Col>
-            <Form.Control name="prompt" type="text" placeholder="Type your message"/>
-          </Col>
-          <Col xs="auto">
-            <Button onClick={submitPrompt} variant="primary" type="button">Send</Button>
-          </Col>
-        </Row>
-      </Form>
+      <div className="images-container">
+        {/* <img src={SergeiImage} alt="Sergei" className="image" />
+        <img src={AlekImage} alt="Alek" className="image" /> */}
+      </div>
+        <div className="messages-container">
+          <Container>
+        {messages.map((message, key) => (
+            <Chat userPrompt={message[0]} user={message[1]} key={key}/>
+        ))}
+          </Container>
+        </div>
+      <div className="message-input">
+  <Form>
+    <Row noGutters className="input-row"> 
+      <Col></Col>
+      <Col xs={6}>
+        <Form.Control name="prompt" type="text" placeholder="Type your message"/>
+      </Col>
+      <Col xs="auto">
+        <Button onClick={submitPrompt} variant="primary" type="button">Send</Button>
+      </Col>
+      <Col></Col>
+    </Row>
+  </Form>
+</div>
     </Container>
   );
 
--- src/Chat.js
+++ src/Chat.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Chat.css';
 
 function Chat(props) {
     
     var person;
     console.log(props)
-    if (props.user == true) {
+    if (props.user === true) {
         person = "Human :"
     }
     else {
         person = "AI :"
     }
 
-
+    const messageClass = props.user ? "user-message" : "bot-message";
 
     return (
-        <div>
-            <h1>{person} {props.userPrompt}</h1>
+        <div className={messageClass}>
+            <h1 style= {{ fontSize: '24px', textAlign: 'left' , paddingLeft: '1em' }} >{person} {props.userPrompt}</h1>
         </div>
     )
 }
