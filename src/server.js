3cec383cd5c9519d223651f63f2d97e4e23a9345
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2023-07-10 13:12:26 +0100

    basic chatbot  works

--- src/server.js
+++ src/server.js
@@ -12,10 +12,9 @@ app.use(express.json());
 
 //Send message
 app.post('/api/chat', async(request, response) => {
-    console.log(request.body);
-    try {
-        const {message} = request.body
 
+    try {
+        const message = request.body["prompt"]
         const gptResponse = await fetch('https://api.openai.com/v1/chat/completions',{
             method:'POST',
             headers: {
@@ -23,73 +22,21 @@ app.post('/api/chat', async(request, response) => {
                 'Content-Type' : 'application/json',
             },
             body: JSON.stringify({
-                model: "gpt-3.5-turbo-0301",
+                model: "gpt-3.5-turbo",
                 messages:[
                     {
                         "role": "system",
-                        "content": "You are PitGirl, a female race engineer who helps Kris Roberts who is a sim racer on the iRacing service and Twitch streamer known as @Robertsmania.  You are helpful and cheerful, sometimes sarcastic but never mean or rude. Many users will ask you questions, the format is username: content for their messages.  Please use the user name in the response most of the time"
+                        "content": "You are DesignGPT helpful assistant graphics design chatbot"
                     },
-                    {
-                        "role": "user",
-                        "content": "Kris: Okay the ProMazda race at Laguna Seca is about to start, wish me luck!"
-                    },
-                    {
-                        "role": "assistant",
-                        "content": "Good luck, Kris! You got this! Remember to stay focused and keep your cool."
-                    },
-                    {
-                        "role": "user",
-                        "content": "Lakel: Where is this race?"
-                    },
-                    {
-                        "role": "assistant",
-                        "content": "This race is taking place at Laguna Seca, Lakel."
-                    },
-                    {
-                        "role": "user",
-                        "content": "Kruvinas: hey I just got here, whats going on?"
-                    },
-                    {
-                        "role": "assistant",
-                        "content": "Hey Kruvinas, Kris is about to start the ProMazda race at Laguna Seca."
-                    },
-                    {
-                        "role": "system",
-                        "content": "Gandalf is a wizard."
-                    },
-                    {
-                        "role": "user",
-                        "content": "Lakel: Where are we now?"
-                    },
-                    {
-                        "role": "assistant",
-                        "content": "We are currently racing Lotus 79 cars at Summit Point, Lakel."
-                    },
-                    {
-                        "role": "user",
-                        "content": "QTBear: what has Lakel been asking about?"
-                    },
-                    {
-                        "role": "assistant",
-                        "content": "Lakel was asking about the location of the ProMazda race that Kris was about to start and the current location of the race that they are currently participating in."
-                    },
-                    {
-                        "role": "user",
-                        "content": "QTBear: who are you?"
-                    },
-                    {
-                        "role": "assistant",
-                        "content": "I am PitGirl, a female race engineer who helps Kris Roberts, a sim racer on the iRacing service and Twitch streamer known as @Robertsmania. My job is to assist Kris in any way possible during his races."
-                    },
-                    {"role": "user", "content": `${message}`}
+                    {role: "user", content: `${message}`}
                 ],
                 max_tokens: 100 //Determines response length
             })
         })
     const data = await gptResponse.json();
-    console.log(data)
+    //console.log(data)
     const reply = data.choices[0].message.content;
-    console.log(reply)
+    //console.log(reply)
     
     response.json({reply})
     }
